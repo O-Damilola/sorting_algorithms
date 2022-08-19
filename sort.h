204cd673925f51a2bb04e9ee0d8254d9208bdf94
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-08-19 09:04:27 +0100

    header files

--- sort.h
+++ sort.h
@@ -1,3 +1,27 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+
 void Swap(int *i, int *j);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
@@ -26,3 +50,6 @@ void sort_down(int *arr, int low, int high);
 void recursion(int *arr, int low, int high, int bool, size_t size);
 void bitonic_sort(int *array, size_t size);
 void print_list(const listint_t *list);
+
+
+#endif /* SORT_H */
